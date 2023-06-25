52fefc11b3256d0cda867bb48b5047dd31e14a43
ThinkPad <ThinkPad@DESKTOP-UT0QOL2>
2023-06-25 23:26:32 +0300

    Model ve Data  katmanı oluşturuldu

---
+++ ErpaTask.Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using ErpaTask.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErpaTask.Data
+{
+	public class AppDbContext:DbContext
+	{
+
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+
+
+		}
+		public virtual DbSet<User>Users { get; set; }	
+		public virtual DbSet<Bank> Banks { get; set; }
+		public virtual DbSet<Account> Accounts { get; set; }	
+
+
+	}
+}
